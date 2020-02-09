0534d8b46e10ddf472d82d3e3d932c8613e4445b
Florin <harabagiufl@gmail.com>
2020-02-09 11:58:31 +0200

    Updates

---
+++ precache-manifest.15ae0006c3c8df9e58dba2176b7dfb95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d230e4bd5dcba234f6bef04cfc4552ad",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "ac7cd65d76f49ac3f86c",
+    "url": "/TicTacToe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "0dbebedd6bd744c08367",
+    "url": "/TicTacToe/static/js/2.185018cc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/TicTacToe/static/js/2.185018cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac7cd65d76f49ac3f86c",
+    "url": "/TicTacToe/static/js/main.b54b262c.chunk.js"
+  },
+  {
+    "revision": "067b2440c069d244bcc4",
+    "url": "/TicTacToe/static/js/runtime-main.31515a8f.js"
+  }
+]);
